e2e90d596cf303d66b8e6a50426af792cd49af73
jlarsen3 <jlarsen3@hills.ccsf.cc.ca.us>
2014-12-17 17:47:03 -0800

    Found a better version of the scratch-paper/doodle code. It's a Doodle.java file that uses a DoodleView.java (I'll have to check if the view one uploaded - they should both have the same date).
    I didn't plug it into the main program - I'm concerned I might break
    something. James, can you take a look at that? I know it needs a method
    to create the Doodle as a new intent and start the activity, but adding
    the xml for the action bar button is where I'm stumped.

--- src/DoodleView.java
+++ src/DoodleView.java
@@ -1,7 +1,182 @@
 /* DoodleView.java
- Main View for the Doodlz app.
- Note: code from the book Android for Programmers: an App-Driven Approach
+http://code.tutsplus.com/tutorials/android-sdk-create-a-drawing-app-interface-creation--mobile-19021
+http://code.tutsplus.com/tutorials/android-sdk-create-a-drawing-app-touch-interaction--mobile-19202
+http://code.tutsplus.com/tutorials/android-sdk-create-a-drawing-app-essential-functionality--mobile-19328
 */
+
+package com.mathproblemconstructor;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.util.TypedValue;
+
+public class DoodleView extends View {
+
+    //drawing path
+    private Path drawPath;
+    //drawing and canvas paint
+    private Paint drawPaint, canvasPaint;
+    //initial color
+    private int paintColor = 0xFF660000;
+    //canvas
+    private Canvas drawCanvas;
+    //canvas bitmap
+    private Bitmap canvasBitmap;
+    //erase flag
+    private boolean erase=false;
+    private float brushSize, lastBrushSize;
+
+
+    public DoodleView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        setupDrawing();
+    }
+
+
+    private void setupDrawing(){
+
+        //prepare for drawing and setup paint stroke properties
+        brushSize = getResources().getInteger(R.integer.medium_size);
+        lastBrushSize = brushSize;
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(brushSize);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    //size assigned to view
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    //draw the view - will be called after touch event
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    //register user touches as drawing action
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+        //respond to down, move and up events
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawPath.lineTo(touchX, touchY);
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+        //redraw
+        invalidate(); //this causes the onDraw() method to execute
+        return true;
+
+    }
+
+    //update color
+    public void setColor(String newColor){
+        invalidate();
+        paintColor = Color.parseColor(newColor);
+        drawPaint.setColor(paintColor);
+    }
+
+    //set erase true or false
+    public void setErase(boolean isErase){
+        erase=isErase;
+        if(erase) drawPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        else drawPaint.setXfermode(null);
+    }
+    //set brush size
+    public void setBrushSize(float newSize){
+        float pixelAmount = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                newSize, getResources().getDisplayMetrics());
+        brushSize=pixelAmount;
+        drawPaint.setStrokeWidth(brushSize);
+    }
+
+    //get and set last brush size
+    public void setLastBrushSize(float lastSize){
+        lastBrushSize=lastSize;
+    }
+
+    public float getLastBrushSize(){
+        return lastBrushSize;
+    }
+
+    //start new drawing
+    public void startNew(){
+        drawCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
+        invalidate();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 import java.util.HashMap;
 import java.util.Map;
 
@@ -99,7 +274,7 @@ public class DoodleView extends View
       // may be related to removed code      
       /*if (singleTapDetector.onTouchEvent(event))
          return true;*/
-      
+ /*
       // get the event type and the ID of the pointer that caused the event
       int action = event.getActionMasked(); // event type 
       int actionIndex = event.getActionIndex(); // pointer (i.e., finger)
@@ -200,3 +375,4 @@ public class DoodleView extends View
       bitmapCanvas.drawPath(path, paintLine); // draw to bitmapCanvas
       path.reset(); // reset the Path
    } 
+*/
